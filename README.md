159b92a472a5ca53812156a5ff0ee12b3bbfde41
SteveChuang <a0911558945@gmail.com>
2019-07-02 23:26:20 +0800

    String串接註解

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Java8LambdasExercise
 Exercise java 8 by steve
+
+
+## lambda
+- Predicate方法使用  
+**先創建一個類似function的感覺**  
+```java
+    Predicate<String> testPredicate = x -> x.length()>=5;
+    if(testPredicate.test(testString)){
+        System.out.println(testString + "length > 5");
+    }
+```java
+```   
+
+
+
+
+
+## Java基本功能
+- String串接  
+```java  
+    //------------------------------------------------------
+    String str = "1";
+    str +="123";
+    //------------------------------------------------------
+    StringBuffer strBuffer = new StringBuffer();
+    strBuffer.append("123");
+    //------------------------------------------------------
+    StringBuilder strBuilder = new StringBuilder();
+    strBuilder.append("123");
+```   
+```
+for迴圈跑十萬次
+StringAdd         5481ms
+StringBuffer      14ms
+StringBuilder     7ms
+```
+
+**測試結論  雖然java預設string串接使用StringBuilder物件所以若不是for迴圈其實沒什麼差別 但如果是for迴圈跑 會一直new StringBuilder物件**
--- src/main/java/steve/lambdaTest.java
+++ src/main/java/steve/lambdaTest.java
@@ -1,18 +1,16 @@
-package steve;                                                                                                                                                            
-                                                                                                                                                                          
-import java.util.function.Predicate;                                                                                                                                      
-                                                                                                                                                                          
-public class lambdaTest {                                                                                                                                                 
-                                                                                                                                                                          
-	public static void main(String[] args) {      
-		String testString = "Steve老大";
-		Predicate<String> testPredicate = x -> x.length()>=5;                                                                                                      
-		                      
-		if(testPredicate.test(testString))
-			System.out.println(testString +"---五個字以上");                                                                                                                    
-		                                                                                                                                                                  
-		                                                                                                                                                                  
-	}                                                                                                                                                                     
-                                                                                                                                                                          
-}                                                                                                                                                                         
-                                                                                                                                                                          
+package steve;
+
+import java.util.function.Predicate;
+
+public class lambdaTest {
+
+	public static void main(String[] args) {
+		String testString = "SteveChuangTest";
+		Predicate<String> testPredicate = x -> x.length() >= 5;
+
+		if (testPredicate.test(testString))
+			System.out.println(testString + "length>5");
+
+	}
+
+}
